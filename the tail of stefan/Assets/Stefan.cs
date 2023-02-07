95b5f5805736c1cc8fea9ac699b12b1d10f6b176
Walllacker <124525994+Walllacker@users.noreply.github.com>
2023-02-07 19:00:28 +0100

    stefan

---
+++ the tail of stefan/Assets/Stefan.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sefancharakter : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float richtung = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+    }
+}
