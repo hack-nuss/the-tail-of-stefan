a747fb7681f062b281173c215f2c013b578be78d
Walllacker <124525994+Walllacker@users.noreply.github.com>
2023-02-07 19:50:03 +0100

    new stefan controls

--- the tail of stefan/Assets/Scripts/Stefan.cs
+++ the tail of stefan/Assets/Scripts/Stefan.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class Stefan : MonoBehaviour
 {
     public float speed = 5;
-    public Rigidbody2D Rigidbody;
-
+    private Rigidbody2D Rigidbody;
+    public float jumph;
+    private bool isgrounded = false;
     // Start is called before the first frame update
     void Start()
     {
-
+        Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,5 +19,18 @@ public class Stefan : MonoBehaviour
     {
         float richtung = Input.GetAxis("Horizontal");
         transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            Rigidbody.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
+            isgrounded = false;
+        }
     }
-}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            isgrounded = true;
+        }
+    }
+}             
