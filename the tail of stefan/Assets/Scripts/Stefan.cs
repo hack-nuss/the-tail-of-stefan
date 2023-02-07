33e612c12eb8b546e86c17450367c0b723585d3c
Walllacker <124525994+Walllacker@users.noreply.github.com>
2023-02-07 20:58:55 +0100

    suck

--- the tail of stefan/Assets/Scripts/Stefan.cs
+++ the tail of stefan/Assets/Scripts/Stefan.cs
@@ -8,23 +8,39 @@ public class Stefan : MonoBehaviour
     private Rigidbody2D Rigidbody;
     public float jumph;
     private bool isgrounded = false;
+    private Vector3 rotation;
+
+    public GameObject kamera;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody2D>();
+        rotation = transform.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
         float richtung = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+
+        if (richtung > 0)
+        {
+            transform.eulerAngles = rotation -new Vector3(0, 180 ,0);
+            transform.Translate(Vector2.right * speed * -richtung * Time.deltaTime);
+        }
+        if (richtung < 0)
+        {
+            transform.eulerAngles = rotation;
+            transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+        }
+        
 
         if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
         {
             Rigidbody.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
             isgrounded = false;
         }
+        kamera.transform.position = new Vector3(transform.position.x, 0, -10);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
