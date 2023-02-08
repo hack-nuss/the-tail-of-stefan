99d8f0e30cab246bda0f065bbf19dded90f3ccad
Nuss <pab8@web.de>
2023-02-08 18:19:45 +0100

    better playermovement script

--- Stefan.cs
+++ Stefan.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Stefan : MonoBehaviour
 {
@@ -10,18 +11,27 @@ public class Stefan : MonoBehaviour
     private bool isgrounded = false;
     private Vector3 rotation;
 
-    public GameObject kamera;
+    PlayerInput playerInput;
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    public new GameObject camera;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody2D>();
         rotation = transform.eulerAngles;
+
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions["move"];
+        jumpAction = playerInput.actions["jump"];
     }
 
     // Update is called once per frame
     void Update()
     {
         float richtung = Input.GetAxis("Horizontal");
+        //var richtung = moveAction.ReadValue<Vector2>();
 
         if (richtung > 0)
         {
@@ -33,14 +43,17 @@ public class Stefan : MonoBehaviour
             transform.eulerAngles = rotation;
             transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
         }
-        
+
 
         if (Input.GetKeyDown(KeyCode.Space) && isgrounded)
         {
             Rigidbody.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
             isgrounded = false;
         }
-        kamera.transform.position = new Vector3(transform.position.x, 0, -10);
+
+        //camera move
+        camera.transform.position = new Vector3(transform.position.x, 0, -10);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
